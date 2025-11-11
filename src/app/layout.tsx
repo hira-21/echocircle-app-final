9dca2c10f5afc4fe1100d9b19a05f2616c797e31
hira-21 <hirasaleemmubashar@gmail.com>
2025-11-11 17:26:10 +0400

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+// src/app/layout.tsx
+import "./styles/globals.css";
+import type { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
