87ef6e26bac08658d75708eeaea0e1ba17912147
hira-21 <hirasaleemmubashar@gmail.com>
2025-11-15 23:46:54 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
-    kotlin("android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+
